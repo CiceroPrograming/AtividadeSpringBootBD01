03beabf2a3af28a3fd4d5d340c1ed888a1d8a132
Cícero <cicero.ferro.78@gmail.com>
2023-05-11 19:20:01 -0300

    primeiro commit

---
+++ src/main/java/com/br/atividadespringbootbd01/atividadespringbootbd01/Atividadespringbootbd01Application.java
@@ -0,0 +1,13 @@
+package com.br.atividadespringbootbd01.atividadespringbootbd01;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Atividadespringbootbd01Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Atividadespringbootbd01Application.class, args);
+	}
+
+}
